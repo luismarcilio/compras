ac47e86085494bf1a2d73537b6911d90dc6415ec
Luis Marcilio Braga de Oliveira <luis.marcilio@redknee.com>
2019-05-10 08:30:08 -0300

    database structure

---
+++ src/test/java/com/luismarcilio/compras/compras/ComprasApplicationTests.java
@@ -0,0 +1,16 @@
+package com.luismarcilio.compras.compras;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ComprasApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
